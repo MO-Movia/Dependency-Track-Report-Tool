5be0d03fafc0689723ca4b4d8cc25b1cf253d7ab
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2021-05-21 13:07:49 +0530

    License column has unintended contents.

--- jenkins-plugin/src/main/java/com/modusoperandi/utility/Processor.java
+++ jenkins-plugin/src/main/java/com/modusoperandi/utility/Processor.java
@@ -420,7 +420,7 @@ public class Processor {
 			// like an indication that there was a translation, and would like both the
 			// original license name and the translated license name in the log
 			// file (being that neither is valid).
-			values[5] += libAudit.isCompliant ? OK_TEXT
+			values[5] = libAudit.isCompliant ? OK_TEXT
 					: ((libAudit.isCommercial ? "Note" : "Invalid") + " [" + libAudit.license + "]"
 							+ ((null != libAudit.inValidXlateLicense)
 									? " Invalid Translated License [" + libAudit.inValidXlateLicense + "]"
